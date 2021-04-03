0985f72d525dc53cdb04a20dc578bff4b313af35
Radu Zamfir <zamfirr75@gmail.com>
2021-04-03 13:51:55 +0300

    fixiiinggg

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,19 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: "AWS_REGION",
-      params: {
-        Bucket: "AWS_BUCKET",
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      upload: {
+        provider: "aws-s3",
+        providerOptions: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: "AWS_REGION",
+          params: {
+            Bucket: "AWS_BUCKET",
+          },
+        },
       },
-    },
-  },
-});
+    };
+  }
+
+  return {};
+};
